54f96628b6658e64a47f52169f38b24b8203c7ac
Joerg Diekmann <joergd@pobox.com>
2012-06-19 10:18:08 +0200

    Adding back some 'standard' middleware that a standard express setup would use ...

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ var roomObj = {};
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
