4320da44287cd653b0cacf664e39675b004aea2e
Richard Archer <richard.archer@unboxedconsulting.com>
2012-06-19 23:43:07 +0200

    Finish pending tests

--- test/test-app.js
+++ test/test-app.js
@@ -1,6 +1,4 @@
-// var should = require('should');
 var io = require('socket.io-client');
-var lobby = require('../lib/lobby.js');
 var socketURL = "http://0.0.0.0:5000";
 var options = {
   transports: ['websocket'],
@@ -10,73 +8,53 @@ var options = {
 describe("Socket Server", function() {
   it('Should not allow users to join non-existant rooms', function(done){
     var client1 = io.connect(socketURL, options);
-    var room1 = 'Room1';
-    client1.on('connect', function(){
-      client1.emit('join room', room1, function(response){
-        response.should.equal('room does not exist');
-        client1.disconnect();
-        done();
-      })
+    var fakeroom = 'Room';
+    client1.emit('join room', fakeroom, function(callback){
+      callback.should.equal('room does not exist');
+      client1.disconnect();
+      done();
     });
   });
 
-  it("Should allow users to create a room", function(done){
+  it("Should allow users to create and join a room", function(done){
     var client1 = io.connect(socketURL, options);
-
-    client1.on('connect', function(){
-      client1.emit('create room', {}, function(response) {
-        response.should.have.property('administratorSet', false);
-        response.should.have.property('room');
+    client1.emit('create room', {}, function(roomUrl) {
+      client1.emit('join room', roomUrl, function(callback){
+        callback.should.have.property('clientcount', 1);
         client1.disconnect();
         done();
       });
     });
   });
 
-  it("Should allow users to join an existing room", function(done){
+  it("Should update clientcount on connect and disconnect", function(done){
     var client1 = io.connect(socketURL, options);
-    
-    // var client3 = io.connect(socketURL, options);
-    client1.on('connect', function(){
-      client1.emit('create room', {}, function(response) {
+    client1.emit('create room', {}, function(roomUrl){
+      client1.emit('room info', roomUrl, function(callback){
+        // client1 has yet to join room, so no admin is set and clientcount is zero
+        Object.keys(callback).length.should.equal(3);
+        callback.should.have.property('clientcount', 0);
 
-        var client2 = io.connect(socketURL, options);
-        client2.on('connect', function(){
-          client2.emit('join room', response.room, function(callback){
-            callback.should.have.property('room', response.room);
 
-            client1.disconnect();
-            client2.disconnect();
-            done();
-          });
-        });
-      });
-    });
-  });
-
-  it("Should assign a count of users in a room", function(done){    
-    var client1 = io.connect(socketURL, options);
-    
-    // var client3 = io.connect(socketURL, options);
-    client1.on('connect', function(){
-      client1.emit('create room', {}, function(response) {
-
-        var client2 = io.connect(socketURL, options);
-        client2.on('connect', function(){
-          client2.emit('join room', response.room, function(callback){
-            callback.should.have.property('room', response.room);
+        client1.emit('join room', roomUrl, function(callback){
+          client1.emit('room info', roomUrl, function(callback){
+            // client1 has now joined room, so _now_ room info is updated
+            Object.keys(callback).length.should.equal(3);
             callback.should.have.property('clientcount', 1);
 
-            var client3 = io.connect(socketURL, options);
-            client3.on('connect', function() {
-              client3.emit('join room', response.room, function(callback){
-                callback.should.have.property('room', response.room);
+            var client2 = io.connect(socketURL, options);
+            client2.emit('join room', roomUrl, function(callback){
+              client2.emit('room info', roomUrl, function(callback){
                 callback.should.have.property('clientcount', 2);
 
                 client1.disconnect();
-                client2.disconnect();
-                client3.disconnect();
-                done();
+                client2.emit('room info', roomUrl, function(callback){
+                  callback.should.have.property('clientcount', 1);
+
+                  client2.disconnect();
+                  done();
+
+                });
               });
             });
           });
@@ -85,23 +63,49 @@ describe("Socket Server", function() {
     });
   });
 
-  it("Should specific room information upon request", function(done){
-    // on('room info')
-    // client count should work
-  });
+  it("Should assign a specific count of users across multiple rooms" ,function(done){
+    var client1 = io.connect(socketURL, options);
+    client1.emit('create room', {}, function(roomUrl1) {
+      client1.emit('join room', roomUrl1, function(callback){
+        callback.should.have.property('clientcount', 1);
+        client1.disconnect();
 
-  it("Should assign a count of users in multiple rooms" ,function(done){
-    //TODO: deal with multiple rooms
-  });
+        var client2 = io.connect(socketURL, options);
+        client2.emit('create room', {}, function(roomUrl2) {
+          client2.emit('join room', roomUrl2, function(callback){
 
-  it("Should update the count of users in a room when users disconnect", function(done){
-    //TODO: deal with disconnects
+            var client3 = io.connect(socketURL, options);
+            client3.emit('join room', roomUrl2, function(callback) {
+
+              callback.should.have.property('clientcount', 2);
+              client2.disconnect();
+              client3.disconnect();
+              done();
+            });
+          });
+        });
+      });
+    });
   });
 
   it("Should assign the first visitor to a room as the administrator", function(done){
-    // on('connect')
-    // if clientcount === 0 roominfo should return administratorSet as false
-    // on ('join room')
-    // if clientcount === 1 roominfo should return administrator as true
-  });  
+    var client1 = io.connect(socketURL, options);
+    client1.emit('create room', {}, function(roomUrl){
+      client1.emit('room info', roomUrl, function(callback){
+        callback.should.have.property('createAdmin', true);
+        callback.should.have.property('hasAdmin', true);
+        callback.should.have.property('clientcount', 0);
+        client1.emit('join room', roomUrl, function(callback){
+          client1.emit('room info', roomUrl, function(callback){
+            callback.should.have.property('createAdmin', false);
+            callback.should.have.property('hasAdmin', true);
+            callback.should.have.property('clientcount', 1);
+            client1.disconnect();
+            done();
+          });
+        });
+      });
+    });
+  });
+
 });
