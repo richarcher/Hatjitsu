3c179971487aacf103112ddc5f5b70a77f903cdf
Joerg Diekmann <joergd@pobox.com>
2012-06-23 12:53:00 +0200

    Trying to get socket errors to appear in the view - but not quite working yet. Also renamed the app to Hatchet for now - just 'cos it's much cooler :-)

--- app/js/services.js
+++ app/js/services.js
@@ -5,12 +5,45 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('pokerApp.services', []).
-  value('version', '0.1');
+var pokerAppServices = angular.module('pokerApp.services', []);
 
-angular.module('pokerApp.services', [], function($provide) {
-  $provide.factory('socket', function() {
-    var socket = io.connect(document.location.origin);
-    return socket;
-  });
-});
+pokerAppServices.value('version', '0.1');
+
+pokerAppServices.service('socket', ['$rootScope', function($rootScope) {
+  var sock = new Sock($rootScope);
+  return sock;
+}]);
+
+
+var Sock = function(rootScope) {
+  this.rootScope = rootScope;
+  this.message = null;
+
+  this.socket = io.connect(document.location.origin);
+
+  this.socket.on('error', function(reason) {
+    this.message = reason;
+    console.log(reason);
+  })
+  this.socket.on('connect_failed', function(reason) {
+    this.message = reason;
+    console.log(reason);
+  })
+  this.socket.on('disconnect', function() {
+    this.message = "Disconnected";
+    console.log('disconnected');
+  })
+  this.socket.on('connect', function() {
+    this.message = null;
+    console.log('connected');
+  })
+};
+
+Sock.prototype.emit = function(msg, data, callback) {
+  this.socket.emit(msg, data, callback);  
+}
+
+Sock.prototype.on = function(msg, callback) {
+  this.message = null;
+  this.socket.on(msg, callback);
+}
