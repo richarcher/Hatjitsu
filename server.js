e941313d6ef88568646cc1c740dfb9826291de31
Joerg Diekmann <joergd@pobox.com>
2012-07-02 18:40:52 +0200

    Add caching to files in /app …. not sure whether this will work.

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.configure('development', function(){
 });
 
 app.configure('production', function(){
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/app', { maxAge: oneYear }));
   app.use(express.errorHandler());
 });
 
