7cb4ae4ddc487646cc25eb4a31a48bf68491a94e
Richard Archer <richard.archer@unboxedconsulting.com>
2012-12-13 11:25:23 +0200

    Degrade nicely for browsers that don't support :checked

--- app/js/testing.js
+++ app/js/testing.js
@@ -1,3 +1,16 @@
+Modernizr.addTest('checked', function(){
+  return Modernizr.testStyles("#modernizr div {width:10px;} #modernizr input:checked ~ div {width: 20px;}", function (elem) {
+    var chx = document.createElement('input'),
+        div = document.createElement('div');
+
+    chx.setAttribute("type","checkbox");
+    chx.setAttribute("checked","checked");
+    elem.appendChild(chx);
+    elem.appendChild(div);
+
+    return elem.lastChild.offsetWidth > 10;
+  });
+});
 
 Modernizr.load([
   {
