9d69ac28e946eb3c4bad4ab661cd254644c0e93e
Rytikov Maksim <tayamarn@yandex-team.ru>
2016-03-09 14:44:39 +0300

    Add yet another card pack

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -169,6 +169,7 @@ function RoomCtrl($scope, $routeParams, $timeout, socket) {
     var seq = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '?'];
     var play = ['A\u2660', '2', '3', '5', '8', '\u2654'];
     var tshirt = ['XL', 'L', 'M', 'S', 'XS', '?'];
+    var seq2 = ['0', '\u00BD', '1', '2', '3', '4', '5', '10', '\u221E', '?'];
     switch (val) {
     case ('fib'):
       return fib;
@@ -180,6 +181,8 @@ function RoomCtrl($scope, $routeParams, $timeout, socket) {
       return play;
     case ('tshirt'):
       return tshirt;
+    case ('seq2'):
+      return seq2;
     default:
       return [];
     }
